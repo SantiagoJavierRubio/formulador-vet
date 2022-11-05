d1a7bccc4cf2fd02db9133cf106727d243e52b47
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-11-04 22:09:05 -0300

    pet list style

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,14 +1,25 @@
 import type { Patient } from "../../utils/types/Patient"
 import styles from "../../styles/Dashboard.module.css";
-import { MdPets } from "react-icons/md"
+import { MdPets } from "react-icons/md";
+import Cat from "../../assets/Cat.svg";
+import Dog from "../../assets/Dog.svg";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Dashboard({ patients }: { patients?: Patient[] }) {
   return (
     <div className={styles.container}>
         {patients && patients.map(patient => {
             return (
-                <div className={styles.card} key={patient.id}>{patient.name} - {patient.species}</div>
+                <div className={styles.card} key={patient.id}>
+                    <h1 className="z-10 text-3xl relative break-before-auto">{patient.name}</h1>
+                    <Image 
+                        src={patient.species === "cat" ? Cat : Dog}
+                        alt={`${patient.species} logo`}
+                        height="100"
+                        className={styles.coverImage}
+                    />
+                </div>
             )
         })}
         <Link href="add-patient" className={styles.card}>
